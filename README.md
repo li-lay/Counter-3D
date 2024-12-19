7bc22a90c84077e6b742e702fc75869805778ed8
LILAY <contact@lilay.dev>
2024-12-19 20:37:23 +0700

    Initial Project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Counter 3D
+
+Counter but in 3D
+
+Library used:
+- three
+- @react-three/fiber
+- @react-three/drei
---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { Canvas } from "@react-three/fiber";
+import { Text3D, Loader, PresentationControls, Center, Environment, ContactShadows } from '@react-three/drei'
+import Kanit from "./assets/fonts/Kanit_Regular.json"
+import { useState, useRef } from "react";
+
+// TODO:
+// 1.Load texture to text (optional) - DROPPED
+// 2.Drag reset position - DONE
+// 3.Center mesh on changes
+// 4.Put on Github Page
+
+function App() {
+
+  const meshRef = useRef()
+
+  const [count, setCount] = useState(0);
+  const IncrementCount = () => {
+    setCount(c => c + 1)
+    // console.log("Position: " + JSON.stringify(meshRef.current.position))
+  }
+  const DecrementCount = () => {
+    setCount(c => c - 1)
+  }
+  const ResetCount = () => {
+    setCount(0)
+  }
+
+  // Move object a bit
+  window.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    // let coor = "Coordinates: (" + x + "," + y + ")";
+    if (meshRef.current) {
+      meshRef.current.position.x = (0 - (x / (window.innerWidth * 10)))
+      meshRef.current.position.y = -(0 - (y / (window.innerHeight * 10)))
+    }
+    // console.log(coor)
+  })
+
+  return (
+    <>
+      <div id="canvas">
+        <Canvas shadows camera={{ position: [0, 0, 4], fov: 25 }} style={{ background: '#282828' }} >
+          <ambientLight intensity={0.5} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            shadow-mapSize={2048}
+            castShadow />
+          <PresentationControls
+            global
+            cursor={false}
+            config={{ mass: 2, tension: 500 }}
+            snap={{ mass: 4, tension: 1500 }}
+            rotation={[0, 0.3, 0]}
+            polar={[-Math.PI / 3, Math.PI / 3]}
+            azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+          >
+
+            <group ref={meshRef}>
+              <Center>
+                <Text3D font={Kanit} {...""} castShadow>
+                  {count}
+                  <meshStandardMaterial color="#98971a" />
+                </Text3D>
+              </Center>
+            </group>
+          </PresentationControls>
+
+          <ContactShadows position={[0, -0.5, 0]} opacity={1} scale={10} blur={3} far={4} />
+          <Environment preset="city" />
+
+        </Canvas>
+        <div className="btn-container">
+          <button className="btn" role="button" onClick={() => { IncrementCount() }}>Increment</button>
+          <button className="btn reset" role="button" onClick={() => { ResetCount() }}>Reset</button>
+          <button className="btn" role="button" onClick={() => { DecrementCount() }}>Decrement</button>
+        </div>
+      </div >
+      <Loader />
+    </>
+  )
+}
+
+export default App
