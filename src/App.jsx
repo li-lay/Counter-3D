0412545f921431f1349e30f090ab9532e7a060e2
LILAY <contact@lilay.dev>
2024-12-19 21:50:47 +0700

    life quality improvement (responsiveness)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Canvas } from "@react-three/fiber";
-import { Text3D, Loader, PresentationControls, Center, Environment, ContactShadows } from '@react-three/drei'
+import { Loader, PresentationControls, Center, Environment, ContactShadows } from '@react-three/drei'
+import { useState, useRef, Suspense } from "react";
+import { Text3D } from "@react-three/drei";
 import Kanit from "./assets/fonts/Kanit_Regular.json"
-import { useState, useRef } from "react";
+
 
 // TODO:
 // 1.Load texture to text (optional) - DROPPED
 // 2.Drag reset position - DONE
 // 3.Center mesh on changes
-// 4.Put on Github Page
+// 4.Put on Github Page - DONE
+// 5.Fix dragging on mobile - DONE
+// 6.Reside for mobile devices - DONE
 
 function App() {
 
@@ -37,10 +41,13 @@ function App() {
     // console.log(coor)
   })
 
+  // Scale on mobile
+  const textScale = window.innerWidth <= 500 ? [0.5, 0.5, 0.5] : [1, 1, 1]
+
   return (
     <>
-      <div id="canvas">
-        <Canvas shadows camera={{ position: [0, 0, 4], fov: 25 }} style={{ background: '#282828' }} >
+      <div id="canvas" >
+        <Canvas shadows camera={{ position: [0, 0, 4], fov: 25 }} style={{ background: '#282828', touchAction: "none" }} >
           <ambientLight intensity={0.5} />
           <spotLight
             position={[10, 10, 10]}
@@ -60,10 +67,12 @@ function App() {
 
             <group ref={meshRef}>
               <Center>
-                <Text3D font={Kanit} {...""} castShadow>
-                  {count}
-                  <meshStandardMaterial color="#98971a" />
-                </Text3D>
+                <Suspense fallback={null}>
+                  <Text3D font={Kanit} {...""} castShadow scale={textScale}>
+                    {count}
+                    <meshStandardMaterial color="#98971a" />
+                  </Text3D>
+                </Suspense>
               </Center>
             </group>
           </PresentationControls>
